7bb29e457eefbb00082e0a5c562364c06e643d6b
Tyler Dougherty <tylerdougherty@me.com>
2016-12-06 01:54:50 -0800

    Added data types and functions to direct work

--- haskell/2016/1.hs
+++ haskell/2016/1.hs
@@ -1,6 +1,7 @@
 input = "R3, L5, R2, L1, L2, R5, L2, R2, L2, L2, L1, R2, L2, R4, R4, R1, L2, L3, R3, L1, R2, L2, L4, R4, R5, L3, R3, L3, L3, R4, R5, L3, R3, L5, L1, L2, R2, L1, R3, R1, L1, R187, L1, R2, R47, L5, L1, L2, R4, R3, L3, R3, R4, R1, R3, L1, L4, L1, R2, L1, R4, R5, L1, R77, L5, L4, R3, L2, R4, R5, R5, L2, L2, R2, R5, L2, R194, R5, L2, R4, L5, L4, L2, R5, L3, L2, L5, R5, R2, L3, R3, R1, L4, R2, L1, R5, L1, R5, L1, L1, R3, L1, R5, R2, R5, R5, L4, L5, L5, L5, R3, L2, L5, L4, R3, R1, R1, R4, L2, L4, R5, R5, R4, L2, L2, R5, R5, L5, L2, R4, R4, L4, R1, L3, R1, L1, L1, L1, L4, R5, R4, L4, L4, R5, R3, L2, L2, R3, R1, R4, L3, R1, L4, R3, L3, L2, R2, R2, R2, L1, L4, R3, R2, R2, L3, R2, L3, L2, R4, L2, R3, L4, R5, R4, R1, R5, R3"
 
 
+--Helpers
 killSpace :: [Char] -> [Char]
 killSpace (' ':s) = s
 killSpace s = s
@@ -11,4 +12,16 @@ noComma (',':xs) = noComma xs
 noComma (x:xs) = x:(noComma xs)
 
 
---calc 
+data Vec = Vec {x :: Int, y :: Int, dir :: Char} deriving (Show)
+
+pos (Vec x y _) = [x, y]
+
+
+moves = map noComma (words input)
+
+--coords :: Vec -> [[Char]] -> []
+coords vec [] = pos vec
+coords vec (move:moves) = coords (nextVec vec move) moves
+
+nextVec :: Vec -> [Char] -> Vec
+nextVec vec move = Vec (x vec
