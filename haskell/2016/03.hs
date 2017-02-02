c4bbae22cf00570e23e1e0ee45cf1e8c56641c4a
Tyler Dougherty <tylerdougherty@me.com>
2017-02-01 19:56:07 -0800

    Finished 2016 day 3, part 2
    Additionally, refactored day one to match the schema I ended up using
    for part 2 (traversal idea).

--- haskell/2016/03.hs
+++ haskell/2016/03.hs
@@ -1,12 +1,31 @@
 run = do
-    file <- readFile "3.txt"
-    putStrLn $ show $ tris (lines file)
+    file <- readFile "03.txt"
+    putStrLn . show . countTris . traverseh . intify2 $ lines file
+
+run2 = do
+    file <- readFile "03.txt"
+    putStrLn . show . countTris . traversev . intify2 $ lines file
 
 
-tris :: [[Char]] -> Int
-tris [] = 0
-tris (st:sts) = result + tris sts
-    where result = if validTri (nums st) then 1 else 0
-          nums str = map (\a -> read a :: Int) (words str)
+intify :: [Char] -> [Int]
+intify l = map (read :: String -> Int) $ words l
 
+intify2 :: [[Char]] -> [[Int]]
+intify2 = map intify
+
+traverseh :: [[Int]] -> [Int]
+traverseh = foldr (++) []
+
+traversev :: [[Int]] -> [Int]
+traversev xs = (map head xs) ++ rest
+    where rest = if done then [] else traversev (map tail xs)
+          done = (head $ map tail xs) == []
+
+countTris :: [Int] -> Int
+countTris [] = 0
+countTris xs = result + (countTris $ drop 3 xs)
+    where result = if (validTri $ take 3 xs) then 1 else 0
+
+validTri :: [Int] -> Bool
 validTri [x,y,z] = x < y+z && y < x+z && z < x+y
+validTri _ = False
