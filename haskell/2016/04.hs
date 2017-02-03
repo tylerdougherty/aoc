3eb96242e4a63ee322c3a2c58a5f164ae4b81061
Tyler Dougherty <tdough@amazon.com>
2017-02-03 15:29:27 -0800

    Finished 2016 day 4, part 1

--- haskell/2016/04.hs
+++ haskell/2016/04.hs
@@ -1,6 +1,31 @@
-import Data.Map as Map
+import qualified Data.Map as Map
 import Data.List.Split
+import Data.List
+import Data.Char
 
+main :: IO ()
+main = do
+    file <- readFile "04.txt"
+    putStrLn . show . validIdSum $ lines file
+
+validIdSum :: [[Char]] -> Int
+validIdSum xs = foldr (+) 0 (map validRoom xs)
+
+validRoom :: [Char] -> Int
+validRoom r = if isValid then read roomId :: Int else 0
+    where [name, roomId, checksum] = [filter isLetter f, filter isNumber f, filter isLetter s]
+          [f, s] = splitOn "[" r
+          isValid = hasMaxes checksum (letterMap name)
+
+letterMap :: [Char] -> Map.Map Char Int
+letterMap = foldl (\acc x -> Map.insertWith (+) x 1 acc) Map.empty
+
+hasMaxes :: [Char] -> Map.Map Char Int -> Bool
+hasMaxes [] _ = True
+hasMaxes (k:ks) m = hasThisMax && hasMaxes ks (Map.delete k m)
+    where hasThisMax = k == (head $ sort $ getMaxes m)
+
+getMaxes :: Map.Map Char Int -> [Char]
 getMaxes m = next [] Nothing (Map.toList m)
     where next ks _ [] = ks
           next ks Nothing ((k,v):rest) = next (k:ks) (Just v) rest
@@ -8,18 +33,3 @@ getMaxes m = next [] Nothing (Map.toList m)
               | v < u = next ks (Just u) rest
               | v > u = next [k] (Just v) rest
               | otherwise = next (k:ks) (Just v) rest
-
-validIdSum :: [[Char]] -> Int
-validIdSum xs = foldr (+) 0 (map validRoom xs)
-
-validRoom :: [Char] -> Int
-validRoom r = 
-    [name, id, checksum] = 
---roomName
---roomId
---checksum
---
---letterMap
---hasMaxes
-
-hasMaxes :: Map -> [Char] -> Bool
