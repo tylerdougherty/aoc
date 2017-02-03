1a2d0afdba1bde31fd84f26444ccf2584f3a4bdd
Tyler Dougherty <tylerdougherty@me.com>
2017-02-03 10:08:44 -0800

    Some skeleton types for 2016 day 4, part 1

--- haskell/2016/04.hs
+++ haskell/2016/04.hs
@@ -8,3 +8,18 @@ getMaxes m = next [] Nothing (Map.toList m)
               | v < u = next ks (Just u) rest
               | v > u = next [k] (Just v) rest
               | otherwise = next (k:ks) (Just v) rest
+
+validIdSum :: [[Char]] -> Int
+validIdSum xs = foldr (+) 0 (map validRoom xs)
+
+validRoom :: [Char] -> Int
+validRoom r = 
+    [name, id, checksum] = 
+--roomName
+--roomId
+--checksum
+--
+--letterMap
+--hasMaxes
+
+hasMaxes :: Map -> [Char] -> Bool
