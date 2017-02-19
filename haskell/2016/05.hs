10a17d7a4a3b8803534b62ef072f4a9677aab9a6
Tyler Dougherty <tylerdougherty@me.com>
2017-02-19 15:12:41 -0800

    Finished day 5, parts 1 and 2

--- haskell/2016/05.hs
+++ haskell/2016/05.hs
@@ -1,13 +1,42 @@
 import Data.Hash.MD5
+import Data.Char
 
 main :: IO ()
 main = do
-    putStrLn ""
+    putStrLn $ take 8 ((!! 5) <$> matches "abbhdwsy")
 
---Try using sequence instead of list?
---a = [s | x <- [0..], let s = "abc" ++ show x, let h = hash s, match h]
-findMatch s n = if match h then h else findMatch s (n+1)
-    where h = md5s $ Str (s ++ show n)
+main2 :: IO ()
+main2 = do
+    putStrLn $ fillPass emptyPass goodPairs
+    where emptyPass = replicate 8 '-'
+          pairs     = (\s -> (digitToInt (s !! 5), s !! 6)) <$> matches "abbhdwsy"
+          goodPairs = filter ((<8) . fst) pairs
 
-match ('0':'0':'0':'0':'0':_) = True
-match _ = False
+hashes :: String -> [String]
+hashes s = md5ify . (s ++) . show <$> [(0 :: Integer)..]
+
+md5ify :: String -> String
+md5ify s = md5s $ Str s
+
+match :: String -> Bool
+match s = take 5 s == "00000"
+
+matches :: String -> [String]
+matches s = filter match (hashes s)
+
+replaceAt :: Int -> Char -> String -> String
+replaceAt n x ys = fst split ++ x : (tail $ snd split)
+    where split = splitAt n ys
+
+tryReplace :: Int -> Char -> String -> String
+tryReplace n x ys
+    | ys !! n == '-' = replaceAt n x ys
+    | otherwise = ys
+
+fillPass :: String -> [(Int, Char)] -> String
+fillPass s xs
+    | count == (length s) = s
+    | otherwise = fillPass new (tail xs)
+    where count = length $ filter (/='-') s
+          new   = tryReplace (fst item) (snd item) s
+          item  = head xs
