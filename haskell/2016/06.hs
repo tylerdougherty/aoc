cd908fc3e13e706bc89dd87eb998343874610a59
Tyler Dougherty <tylerdougherty@me.com>
2017-02-19 23:05:38 -0800

    Finished day 6, part 2

--- haskell/2016/06.hs
+++ haskell/2016/06.hs
@@ -6,8 +6,17 @@ main = do
     file <- readFile "06.txt"
     putStrLn . map mostCommon $ inverse . lines $ file
 
+main2 = do
+    file <- readFile "06.txt"
+    putStrLn . map leastCommon $ inverse . lines $ file
+
 mostCommon :: String -> Char
-mostCommon xs = fst (maximumBy (compare `on` snd) $ count)
+mostCommon xs = fst (sortWith maximumBy xs)
+
+leastCommon :: String -> Char
+leastCommon xs = fst (sortWith minimumBy xs)
+
+sortWith f xs = f (compare `on` snd) $ count
     where count = map (head &&& length) (group . sort $ xs)
 
 inverse :: [String] -> [String]
