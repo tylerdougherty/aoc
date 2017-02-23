8ddade8ca4b4f364942dc1ca293877e121d6f325
Tyler Dougherty <tdough@amazon.com>
2017-02-23 14:53:21 -0800

    More work and some ideas on day 8, part 1

--- haskell/2016/08.hs
+++ haskell/2016/08.hs
@@ -1,3 +1,11 @@
+import Data.List
+
 type Grid = [[Char]]
 
 rotate n xs = zipWith const (drop (length xs - n) (cycle xs)) xs
+
+--splitAt?
+--take i xs, drop (i+1) xs
+rotateRow i n xs = rotate n $ xs !! i
+
+rotateCol i n xs = transpose . rotate n . transpose $ xs !! i
