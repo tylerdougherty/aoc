185f9650762f314896d86b15803e9bc17e901cb1
Tyler Dougherty <tylerdougherty@me.com>
2017-03-04 02:48:19 -0800

    Finished day 9, part 2

--- haskell/2016/09.hs
+++ haskell/2016/09.hs
@@ -1,9 +1,19 @@
 import Data.List.Split
+import Data.List.Extra (stripInfix)
+import Data.Maybe
 
 main :: IO ()
 main = do
     file <- readFile "09.txt"
     putStrLn . show . foldl (+) 0 . map (length . parseFirst) $ lines file
+
+main2 :: IO ()
+main2 = do
+    file <- readFile "09.txt"
+    putStrLn . show . foldl (+) 0 . map count . lines $ file
+
+splitOnOne :: String -> String -> (String, String)
+splitOnOne x ys = fromJust $ stripInfix x ys
 
 parseFirst :: String -> String
 parseFirst [] = ""
@@ -13,7 +23,15 @@ parseFirst s
     where a = head $ splitOn "(" s
           next = drop (length a) s
           x = splitOneOf "()" s !! 1
-          chars = read $ splitOn "x" x !! 0
-          reps = read $ splitOn "x" x !! 1
+          [chars, reps] = map read $ splitOn "x" x
           lp = head $ splitOn ")" s
           rp = drop (length lp + 1) s
+
+count :: String -> Int
+count ('(':xs) =
+    let (nums, rest) = splitOnOne ")" xs
+        [chars, reps] = map read $ splitOn "x" nums
+        str = take chars rest
+    in (reps * count str) + count (drop chars rest)
+count (x:xs) = 1 + count xs
+count [] = 0
